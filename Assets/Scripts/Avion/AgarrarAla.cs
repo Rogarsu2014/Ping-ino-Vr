e51ccd5aa172ee5f5e7fe925484f1221a3cee0cb
Rogarsu2014 <rogarsu2014@gmail.com>
2022-04-04 23:14:04 +0200

    Codigo para enseñar en clase

--- Assets/Scripts/Avion/AgarrarAla.cs
+++ Assets/Scripts/Avion/AgarrarAla.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class AgarrarAla : MonoBehaviour
 {
     public MeshRenderer[] ms;
+    public Transform mano;
     private void Start()
     {
         ms = this.GetComponentsInChildren<MeshRenderer>();
@@ -23,5 +25,11 @@ public class AgarrarAla : MonoBehaviour
             r.material.color = Color.blue;
         }
     }
-
+    public void CambiarRot()
+    {
+        XRGrabInteractable i = this.GetComponent<XRGrabInteractable>();
+        Transform j = i.selectingInteractor.transform;
+        this.transform.rotation = this.transform.rotation * j.rotation;
+        //this.transform.rotation = mano.rotation;
+    }
 }
