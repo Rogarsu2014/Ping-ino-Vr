dc3deeb7a6084d4c8c338c8bf4d8c73a8f256cdb
david-3lm <d.3lopm@gmail.com>
2022-04-07 20:32:43 +0200

    Bang

--- Assets/Scripts/MarcoPolo/InputMap.cs
+++ Assets/Scripts/MarcoPolo/InputMap.cs
@@ -5,9 +5,25 @@ using UnityEngine.InputSystem;
 
 public class InputMap : MonoBehaviour
 {
-    public void fire()
+    Vector2 movement;
+    float rotSpeed = 5f;
+    float speed = .2f;
+    public void OnActivate()
     {
         print("Bang");
     }
+
+    public void OnMove()
+    {
+        print("memuevo");
+    }
+
+    void FixedUpdate()
+    {
+        var playerTransform = transform;
+        playerTransform.Rotate(Vector3.up * movement.x * rotSpeed);
+        playerTransform.Translate(Vector3.forward * movement.y * speed);
+    }
+
 
 }
