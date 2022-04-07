552d2d1bf1cbb28bb6b58365908f3c4f7d10b772
david-3lm <d.3lopm@gmail.com>
2022-04-07 13:36:51 +0200

    Inicio de un gran minijuego

---
+++ Assets/Scripts/MarcoPolo/InputMap.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputMap : MonoBehaviour
+{
+    public void fire()
+    {
+        print("Bang");
+    }
+
+}
