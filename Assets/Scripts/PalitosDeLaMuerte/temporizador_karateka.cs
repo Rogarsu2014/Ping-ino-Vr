e423449c4137c66b56487e4a42977458826b0a7b
Pablo Álvarez de Lara Fernández <pablo.alf2001@gmail.com>
2022-05-16 22:53:46 +0200

    intento de temporizador pinguinos karatekas
    He empezado a implementar el temporizador para la partida de pinguinos karatekas.

---
+++ Assets/Scripts/PalitosDeLaMuerte/temporizador_karateka.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class temporizador : MonoBehaviour
+{
+    public float tiempo = 120;
+    public Text textoTiempo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tiempo > 0)
+        {
+            tiempo -= Time.deltaTime;
+        } 
+        else 
+        {
+            tiempo = 0;
+        }
+
+        muestraTiempo(tiempo);
+    }
+
+    void muestraTiempo(float tiempoAMostrar)
+    {
+        if (tiempoAMostrar < 0)
+        {
+            tiempoAMostrar = 0;
+        }
+
+        float minutos = Mathf.FloorToInt(tiempoAMostrar / 60);
+        float segundos = Mathf.FloorToInt(tiempoAMostrar % 60);
+
+        textoTiempo.text = string.Format("{0:0}:{1:00}", minutos, segundos);
+    }
+}
